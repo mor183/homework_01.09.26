f04852abb32a5e114673c264e71de8c9ed5f525b
mor183 <mor183@bk.ru>
2026-09-08 21:23:32 +0300

    Update Address.py

--- Address.py
+++ Address.py
@@ -5,9 +5,10 @@ class Address:
     house = "15"
     flat = "346"
 
-    def __init__(self, index, town, house, flat):
+    def __init__(self, index, town, street, house, flat):
         self.i = index
         self.t = town
+        self.s = street
         self.h = house
         self.f = flat
         
